80040bef750c323690bf30251d4a45027eba4bea
Evgeniy <lagun.evgeniy30@gmail.com>
2020-04-06 22:26:56 +0300

    My SortableShapes solution

---
+++ CodeWarsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeWarsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeWarsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bb92e33-7af0-4969-9d37-4b76f6f9817c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CodeWars_Tasks/SortableShapes/SortableShapes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CodeWars_Tasks.SortableShapes
+{
+    public class Shape : IComparable<Shape>
+    {
+        public double Area { get; set; }
+
+        public int CompareTo(Shape o)
+        {
+            return Area.CompareTo(o.Area);
+        }
+    }
+
+    public class Square : Shape
+    {
+        public Square(double side)
+        {
+            Area = side * side;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle(double width, double height)
+        {
+            Area = width * height;
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public Triangle(double basis, double height)
+        {
+            Area = basis * height / 2;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Area = radius * radius * Math.PI;
+        }
+    }
+
+    public class CustomShape : Shape
+    {
+        public CustomShape(double area)
+        {
+            Area = area;
+        }
+    }
+}
